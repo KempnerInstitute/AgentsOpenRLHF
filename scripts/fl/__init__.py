a86c7e908a0a3001ffa24c0d7123954fa8f3d787
Ellen Ma <ellenzma237@gmail.com>
2025-09-03 09:33:08 -0400

    feat: add prompt utils

--- scripts/fl/__init__.py
+++ scripts/fl/__init__.py
@@ -2,14 +2,14 @@
 from .fl_agent_utils import (
     assert_valid,
     parse_action, parse_sim_actions, parse_tool,
-    make_prompt, make_prompt_sim,
-    env_to_list, env_to_str, grid_list_to_str, str_to_grid_list,
+    make_prompt_no_tool, make_prompt_sim,
+    env_to_list, env_to_str, grid_list_to_str, str_to_grid_list, make_prompt,
     create_gym_env_from_grid, extract_grid_from_prompt,
 )
 __all__ = [
     "assert_valid", "str_to_grid_list", "grid_list_to_str"
     "parse_action", "parse_sim_actions", "parse_tool",
-    "make_prompt", "make_prompt_sim",
+    "make_prompt_no_tool", "make_prompt_sim", "make_prompt"
     "env_to_list", "env_to_str",
     "create_gym_env_from_grid", "extract_grid_from_prompt",
 ]
